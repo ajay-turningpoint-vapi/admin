9e910d67497b5b435095c88f70482349435cba7c
ajay-turningpoint-vapi <ajay@turningpoint.com>
2024-08-29 09:47:36 +0530

    build

--- src/services/url.service.js
+++ src/services/url.service.js
@@ -1,4 +1,4 @@
-export const url = "http://192.168.1.117:4023";
+// export const url = "http://192.168.1.117:4023";
 //live url
-// export const url = "http://13.126.184.197/api";
+export const url = "http://13.126.184.197/api";
 
