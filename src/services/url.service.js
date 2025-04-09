d899246690c1b4e6690262f888bf107e56241e12
ajay-turningpoint-vapi <ajay@turningpointvapi.com>
2025-04-09 12:53:10 +0530

    staging area test ec2

--- src/services/url.service.js
+++ src/services/url.service.js
@@ -2,7 +2,8 @@
 // export const SOCKET_URL = "ws://192.168.1.152:4023";
 
 //live url
-export const url = "http://13.126.184.197";
+// export const url = "http://13.126.184.197";
+
 // export const url = "https://api.turningpointvapi.com/api";
 
-
+export const url="http://3.7.46.178/api/"
