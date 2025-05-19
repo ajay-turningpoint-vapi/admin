c6cc4fdd5f953885ec0a0efb4e60314a1f4e9747
ajay-turningpoint-vapi <ajay@turningpointvapi.com>
2025-05-19 12:42:31 +0530

    new build

--- src/services/url.service.js
+++ src/services/url.service.js
@@ -1,8 +1,8 @@
-export const url = "http://192.168.1.120:4023";
+// export const url = "http://192.168.1.120:4023";
 // export const SOCKET_URL = "ws://192.168.1.152:4023";
 
 //live url
-// export const url = "http://13.126.184.197";
+export const url = "http://13.126.184.197";
 
 // export const url = "https://api.turningpointvapi.com/api";
 
